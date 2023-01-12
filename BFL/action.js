d769f0c15e09cd0d11d3f13645ff1219dbd43075
Angelo M <65518034+Norbory@users.noreply.github.com>
2023-01-12 17:02:36 -0500

    Aumentar Javascript

---
+++ BFL/action.js
@@ -0,0 +1,8 @@
+function muestra(){
+    let clase = document.querySelector(".jugador1");
+    clase.style.display = "flex";
+}
+function oculta(){
+    let clase = document.querySelector(".jugador1");
+    clase.style.display = "none";
+}
